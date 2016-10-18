e17f0285060fa9708c8d3b56ac554e3dc5497d4c
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-18 16:46:43 -0700

    spliting app.js into multiple .js files

---
+++ candyCostumFilter.js
@@ -0,0 +1,11 @@
+angular.module('candyShop')
+//costum made filter initialization
+  .filter('checkmark', checkmark)
+
+
+//costum made filter processing
+function checkmark(){
+  return function(input){
+    return input ? '\u2713':'\u2718'
+  }
+}
---
+++ candyDir.js
@@ -0,0 +1,21 @@
+angular.module('candyShop')
+  .directive('candyList', candyList)
+  .directive('candyFilters', candyFilters)
+
+
+function candyList(){
+  var directive = {
+    // 'E': Accessable HTML Elements; 'A': Accessable Element's Attribute, 'C': Accessable Element's Class
+    restrict: 'E',
+    templateUrl: 'templates/candyList.html'
+  }
+  return directive
+}
+
+function candyFilters(){
+  var directive = {
+  restrict: 'E',
+  templateUrl: 'templates/candyFilters.html'
+}
+return directive
+}
