260cf36c26db8a6e44593eb5db3b0f601aa95439
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-18 15:14:15 -0700

    initial commit

---
+++ candyCtrl.js
@@ -0,0 +1,8 @@
+angular.module('candyShop')
+    .controller('CandyController', CandyController)
+
+    function CandyController() {
+    var vm = this;
+
+    vm.candies=[{name: 'Snickers', color: 'Brown', fruity: false, price: 1.5}, {name: 'Fruit Roll Ups', color: 'Red', fruity: true, price: 3}, {name: 'White Chocolate Hershey Bars', color: 'White', fruity: false, price: .85}, {name: 'Lime Jelly Beans', color: 'Green', fruity: true, price: .05}, {name: 'Red Hots', color: 'Red', fruity: false, price: 1.25}, {name: 'Whoppers', color: 'Brown', fruity: false, price: 1.75}]
+    }
