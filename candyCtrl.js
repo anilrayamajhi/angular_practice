d38f65e9af89bfbdddd795dc30d31bf5bbc7a334
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-18 21:09:55 -0700

    manipulate data with controller comment version backup

--- candyCtrl.js
+++ candyCtrl.js
@@ -1,3 +1,6 @@
+(function(){
+  'use strict';
+
 angular.module('candyShop')
 //startoff with candycontroller
     .controller('CandyController', CandyController)
@@ -5,7 +8,7 @@ angular.module('candyShop')
     function CandyController($http) {
     var vm = this;
     $http({
-      ethod: 'GET',
+      method: 'GET',
       url: 'data.json'
     }).success(function(data){
       console.log(data);
@@ -13,3 +16,14 @@ angular.module('candyShop')
     })
 
 }
+})()
+
+// angular.module('candyShop')
+// //startoff with candycontroller
+//     .controller('CandyController', CandyController)
+//
+//     function CandyController() {
+//     var vm = this;
+//
+//     vm.candies=[{name: 'Snickers', color: 'Brown', fruity: false, price: 1.5}, {name: 'Fruit Roll Ups', color: 'Red', fruity: true, price: 3}, {name: 'White Chocolate Hershey Bars', color: 'White', fruity: false, price: .85}, {name: 'Lime Jelly Beans', color: 'Green', fruity: true, price: .05}, {name: 'Red Hots', color: 'Red', fruity: false, price: 1.25}, {name: 'Whoppers', color: 'Brown', fruity: false, price: 1.75}]
+// }
