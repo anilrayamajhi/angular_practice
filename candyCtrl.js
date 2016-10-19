6ce6b3900c5f5f7bb74ffe7c5322f93d1a401948
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-18 21:05:20 -0700

    get method implementation

--- candyCtrl.js
+++ candyCtrl.js
@@ -1,12 +1,15 @@
-(function(){
-  'use strict';
 angular.module('candyShop')
 //startoff with candycontroller
     .controller('CandyController', CandyController)
 
-    function CandyController() {
+    function CandyController($http) {
     var vm = this;
+    $http({
+      ethod: 'GET',
+      url: 'data.json'
+    }).success(function(data){
+      console.log(data);
+      return vm.candies = data;
+    })
 
-    vm.candies=[{name: 'Snickers', color: 'Brown', fruity: false, price: 1.5}, {name: 'Fruit Roll Ups', color: 'Red', fruity: true, price: 3}, {name: 'White Chocolate Hershey Bars', color: 'White', fruity: false, price: .85}, {name: 'Lime Jelly Beans', color: 'Green', fruity: true, price: .05}, {name: 'Red Hots', color: 'Red', fruity: false, price: 1.25}, {name: 'Whoppers', color: 'Brown', fruity: false, price: 1.75}]
-    }
-})()
+}
